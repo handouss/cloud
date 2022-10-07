3c9feb28ea7f0c2cc1b002ca8adfb4329763da41
handouss <oussamahandous36@gmail.com>
2022-10-07 05:52:05 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+
+import Mycomponent from './component/Mycomponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+<Mycomponent/>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
